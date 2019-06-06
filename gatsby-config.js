b4626387b23f814e432de2da8a400f1e5c3d7ab5
dragonza <zell_2004_2005@yahoo.com>
2019-06-07 00:24:11 +0700

    create sticky header

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,19 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [`gatsby-remark-prismjs`],
+        plugins: [
+          "gatsby-remark-copy-linked-files",
+          `gatsby-remark-prismjs`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
       },
     },
     {
